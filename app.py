2c623a909b862eb4317d732d2d8c5bd53f42dde9
Lily Pandey <panlily2002@gmail.com>
2025-04-25 11:41:50 +0530

    old users can see logs since they connected

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def log_view():
     return render_template('log.html')
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
